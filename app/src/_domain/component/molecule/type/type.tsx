5a6f1c778b41f5460df647035258deae7e4b8d0c
Jieun Lee <easyeun410@gmail.com>
2024-11-13 23:54:07 +0900

    Feat: #3  footer 코드 수정

--- app/src/_domain/component/molecule/type/type.tsx
+++ app/src/_domain/component/molecule/type/type.tsx
@@ -3,9 +3,9 @@ import S from "./style";
 function Type(): JSX.Element {
   return (
     <S.Container>
-      <S.TypeButton sx={{ backgroundColor: "white" }}>개인</S.TypeButton>
+      <S.TypeButton sx={{ backgroundColor: "white" }}>강원</S.TypeButton>
       <S.TypeButton sx={{ borderRight: "1px solid #959595", opacity: 0.7 }}>
-        기업
+        미래24
       </S.TypeButton>
     </S.Container>
   );
